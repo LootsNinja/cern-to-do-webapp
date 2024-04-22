580353dca9c6b0c3e4d65d52b4bc9fe0a91b296c
mabusiam <mansour.abusiam@finago.com>
2024-04-22 22:37:40 +0300

    add home controller to springboot so it serves angular frontend

---
+++ src/main/java/ch/cern/todo/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package ch.cern.todo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller()
+public class HomeController {
+    @RequestMapping(value = "/**/{path:^(?!.*\\.).*$}")
+    public String redirect() {
+        return "forward:/";
+    }
+
+}
